8f956cf7d979d384b581a12b380fc9802f48102c
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-10-28 10:56:50 +0100

    .

--- lwjgl3/src/main/java/comboom/sucht/Jumping/Runner/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/comboom/sucht/Jumping/Runner/lwjgl3/Lwjgl3Launcher.java
@@ -8,8 +8,6 @@ import org.lwjgl.system.Configuration;
 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
-
-    private static final comboom.sucht.Jumping.Runner.MyColor color = new comboom.sucht.Jumping.Runner.MyColor();
     public static void main(String[] args) {
         if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
         createApplication();
@@ -25,7 +23,7 @@ public class Lwjgl3Launcher {
         //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
         //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
         configuration.useVsync(true);
-        configuration.setInitialBackgroundColor(color.black());
+        configuration.setInitialBackgroundColor(MyColor.BLACK);
         //// Limits FPS to the refresh rate of the currently active monitor, plus 1 to try to match fractional
         //// refresh rates. The Vsync setting above should limit the actual FPS to match the monitor.
         configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
