6399c9c00540edf015c256a916ce99e0a807e825
mcpeaps_HD <mcpeaps_HD@outlook.com>
2024-11-28 20:49:14 +0100

    Dev (#22)
    * .
    
    * .
    
    * Update .gitmodules
    
    * .
    
    * .
    
    * .
    
    * m
    
    * .
    
    * Update .gitignore
    
    * .
    
    * .

--- src/jumping-runner.cpp
+++ src/jumping-runner.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "nord-color.hpp"
+#include <vector>
 
 #define SREEN_WIDTH     1920
 #define SREEN_HEIGHT    1080
@@ -14,20 +15,41 @@
 int main(void)
 {
     InitWindow(SREEN_WIDTH, SREEN_HEIGHT, "Jumping Runner");
-    SetWindowState(FLAG_WINDOW_RESIZABLE);
-    SetWindowState(FLAG_VSYNC_HINT);
-    SetWindowState(FLAG_MSAA_4X_HINT);
-    SetWindowState(FLAG_WINDOW_HIGHDPI);
-    SetWindowState(FLAG_INTERLACED_HINT);
     SetTargetFPS(TARGET_FPS);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(GetColor(NORD_BACKGROUND));
+
+        float groundHeight = GetScreenHeight() * GROUND_HEIGHT_PERCENT;
+        DrawRectangle(0, GetScreenHeight() - groundHeight,
+                     GetScreenWidth(), groundHeight, GetColor(NORD_FOREGROUND));
+
+        DrawRectangleRec(game.dino.rect, GetColor(NORD_WHITE));
+
+        for (const auto& obs : game.obstacles) {
+            if (obs.active) {
+                DrawRectangleRec(obs.rect, GetColor(NORD_RED));
+            }
+        }
+
+        DrawTextEx(gameFont, TextFormat("Score: %d", game.score/60),
+                  {10, 10}, 20, 2, GetColor(NORD_WHITE));
+
+        if (game.gameOver) {
+            const char* gameOverText = "Game Over!\nClick to restart";
+            Vector2 textSize = MeasureTextEx(gameFont, gameOverText, 40, 2);
+            DrawTextEx(gameFont, gameOverText,
+                      {GetScreenWidth()/2 - textSize.x/2,
+                       GetScreenHeight()/2 - textSize.y/2},
+                      40, 2, GetColor(NORD_RED));
+        }
+
         EndDrawing();
     }
-    CloseWindow();
 
+    UnloadFont(gameFont);
+    CloseWindow();
     return 0;
 }
